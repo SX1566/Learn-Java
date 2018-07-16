1037c26911bc75f8d2eb356d0f22f97a7f608f1a
RJ.HWang <rongjihuang@gmail.com>
2018-07-16 10:18:17 +0800

    Polishing data4bc.sql

--- gftaxi-traffic-accident-ui-web/src/main/resources/cn/gftaxi/traffic/accident/sql/postgres/data4bc.sql
+++ gftaxi-traffic-accident-ui-web/src/main/resources/cn/gftaxi/traffic/accident/sql/postgres/data4bc.sql
@@ -15,7 +15,7 @@ with p(id) as ( -- 交通事故父目录
 ),
 -- 清除插入的角色
 role(id) as (select id from bc_identity_role where code like 'ACCIDENT_%')
---select * from role r inner join bc_identity_role o on o.id = r.id           -- 查出要删除的角色
+--select * from role r inner join bc_identity_role o on o.id = r.id -- 查出要删除的角色
 , delete_role_actor as (        -- 删除用户与角色的关联
   delete from bc_identity_role_actor where rid in (select id from role)
   returning *
@@ -41,7 +41,7 @@ insert into bc_identity_resource (status_, inner_, type_, order_, name, iconclas
 -- 资源：营运系统/交通事故/*
 with p(id) as (select id from bc_identity_resource where name = '交通事故')
 , cfg(type, sn, name, url, iconclass) as (
-        select 2, '072001', '事故报案'::text, '/static/accident/accident-draft/view.html', 'i0707'
+  select 2, '072001', '事故报案'::text, '/static/accident/accident-draft/view.html', 'i0707'
 )
 insert into bc_identity_resource (status_, inner_, type_, order_, name, url, iconclass, belong, id)
   select 0, false, c.type, c.sn, c.name, c.url, c.iconclass, (select id from p), nextval('core_sequence')
@@ -50,12 +50,12 @@ insert into bc_identity_resource (status_, inner_, type_, order_, name, url, ico
 
 -- 角色
 with cfg(sn, name, code) as (
-  -- 提交报案信息角色
-        select '4011', '提交报案信息角色'::text, 'ACCIDENT_DRAFT_MODIFY'::text
+  -- 查询报案信息角色
+         select '4011', '查询报案信息角色'::text, 'ACCIDENT_DRAFT_READ'::text
   -- 提交报案信息角色
   union select '4012', '提交报案信息角色'::text, 'ACCIDENT_DRAFT_SUBMIT'::text
-  -- 查询报案信息角色
-  union select '4012', '查询报案信息角色'::text, 'ACCIDENT_DRAFT_READ'::text
+  -- 修改报案信息角色
+  union select '4013', '修改报案信息角色'::text, 'ACCIDENT_DRAFT_MODIFY'::text
 )
 insert into bc_identity_role (status_, inner_, type_, order_, code, name, id)
   select 0, false, 0, c.sn, c.code, c.name, nextval('core_sequence')
@@ -66,9 +66,8 @@ insert into bc_identity_role (status_, inner_, type_, order_, code, name, id)
 with p(id) as (
   select id from bc_identity_resource
   where name = '交通事故'
-)
-, cfg(resource_name, role_codes) as (
-        select '事故报案'::text, array['ACCIDENT_DRAFT_MODIFY', 'ACCIDENT_DRAFT_SUBMIT','ACCIDENT_DRAFT_READ']
+), cfg(resource_name, role_codes) as (
+  select '事故报案'::text, array['ACCIDENT_DRAFT_MODIFY', 'ACCIDENT_DRAFT_SUBMIT','ACCIDENT_DRAFT_READ']
 )
 insert into bc_identity_role_resource (rid, sid)
   select r.id, s.id
@@ -78,8 +77,10 @@ insert into bc_identity_role_resource (rid, sid)
 
 -- 用户与角色的关联
 with cfg(role_code, user_codes) as (
+  -- 查询报案信息角色
+  select 'ACCIDENT_DRAFT_READ'::text, array['baochengzongbu']
   -- 提交报案信息角色
-        select 'ACCIDENT_DRAFT_MODIFY'::text,
+  union select 'ACCIDENT_DRAFT_MODIFY'::text,
     array[
       'foy', 'zws', 'zeng', 'jon', 'kelvin', 'owen', 'zsk',
       'lys', 'eagle', 'mars', 'wjb', 'cmy', 'hyp', 'hjx', 'zhong'
@@ -90,8 +91,6 @@ with cfg(role_code, user_codes) as (
       'foy', 'zws', 'zeng', 'jon', 'kelvin', 'owen', 'zsk',
       'lys', 'eagle', 'mars', 'wjb', 'cmy', 'hyp', 'hjx', 'zhong'
     ]
-  -- 查询报案信息角色
-  union select 'ACCIDENT_DRAFT_READ'::text, array['baochengzongbu']
 )
 insert into bc_identity_role_actor (rid, aid)
   select r.id, a.id
