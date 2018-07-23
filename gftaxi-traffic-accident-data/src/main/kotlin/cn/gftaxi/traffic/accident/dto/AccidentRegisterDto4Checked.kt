10df2aa7d0986a81706a071f2a234a45d0e7b0af
RJ.HWang <rongjihuang@gmail.com>
2018-07-23 18:43:14 +0800

    事故登记 Rest 接口实现 - 获取已审核信息部分

---
+++ gftaxi-traffic-accident-data/src/main/kotlin/cn/gftaxi/traffic/accident/dto/AccidentRegisterDto4Checked.kt
@@ -0,0 +1,36 @@
+package cn.gftaxi.traffic.accident.dto
+
+import cn.gftaxi.traffic.accident.po.AccidentRegister
+import cn.gftaxi.traffic.accident.po.AccidentRegister.Status.Approved
+import cn.gftaxi.traffic.accident.po.AccidentRegister.Status.Rejected
+import java.time.OffsetDateTime
+import javax.persistence.Entity
+import javax.persistence.Id
+
+/**
+ * 事故登记的已审核案件 DTO。
+ *
+ * @author RJ
+ */
+@Entity
+data class AccidentRegisterDto4Checked constructor(
+  @Id val code: String,
+  val carPlate: String,
+  val driverName: String,
+  /** 是否非编司机 */
+  val outsideDriver: Boolean,
+  /** 审核结果：[Approved] 或 [Rejected] */
+  val checkResult: AccidentRegister.Status,
+  /** 审核意见 */
+  val checkComment: String?,
+  /** 审核人姓名 */
+  val checkerName: String,
+  /** 审核次数 */
+  val checkedCount: Int,
+  /** 审核时间 */
+  val checkedTime: OffsetDateTime,
+  /** 附件名称 */
+  val attachmentName: String?,
+  /** 附件 ID */
+  val attachmentId: String?
+)
