86015ec66dce5ba6a3e2934f4bd1c1ba299c1249
RJ.HWang <rongjihuang@gmail.com>
2018-08-29 13:40:41 +0800

    Polishing - 简化角色名称和权限配置脚本

--- gftaxi-traffic-accident-ui-web/src/main/resources/cn/gftaxi/traffic/accident/sql/postgres/data4bc.sql
+++ gftaxi-traffic-accident-ui-web/src/main/resources/cn/gftaxi/traffic/accident/sql/postgres/data4bc.sql
@@ -70,16 +70,16 @@ insert into bc_identity_resource (status_, inner_, type_, order_, name, url, ico
 
 -- 角色
 with cfg(sn, name, code) as (
-  -- 事故报案相关角色
-         select '4011', '交通事故查询报案'::text, 'ACCIDENT_DRAFT_READ'::text
-  union select '4012', '交通事故上报案件'::text, 'ACCIDENT_DRAFT_SUBMIT'::text
-  union select '4013', '交通事故修改报案'::text, 'ACCIDENT_DRAFT_MODIFY'::text
+  -- 事故报案
+         select '4011', '事故报案-查询'::text, 'ACCIDENT_DRAFT_READ'::text
+  union select '4012', '事故报案-上报'::text, 'ACCIDENT_DRAFT_SUBMIT'::text
+  union select '4013', '事故报案-修改'::text, 'ACCIDENT_DRAFT_MODIFY'::text
 
-  -- 事故登记相关角色
-  union select '4021', '交通事故登记信息查询'::text, 'ACCIDENT_REGISTER_READ'::text
-  union select '4022', '交通事故报案信息登记'::text, 'ACCIDENT_REGISTER_SUBMIT'::text
-  union select '4023', '交通事故登记信息修改'::text, 'ACCIDENT_REGISTER_MODIFY'::text
-  union select '4024', '交通事故登记信息审核'::text, 'ACCIDENT_REGISTER_CHECK'::text
+  -- 事故登记
+  union select '4021', '事故登记-查询'::text, 'ACCIDENT_REGISTER_READ'::text
+  union select '4022', '事故登记-提交'::text, 'ACCIDENT_REGISTER_SUBMIT'::text
+  union select '4023', '事故登记-修改'::text, 'ACCIDENT_REGISTER_MODIFY'::text
+  union select '4024', '事故登记-审核'::text, 'ACCIDENT_REGISTER_CHECK'::text
 )
 insert into bc_identity_role (status_, inner_, type_, order_, code, name, id)
   select 0, false, 0, c.sn, c.code, c.name, nextval('core_sequence')
@@ -92,11 +92,9 @@ with p(id) as (
   union select id from bc_identity_resource
     where name = '统计报表' and belong = (select id from bc_identity_resource where name = '事故(新版)')
 ), cfg(resource_name, role_codes) as (
-  select '事故报案'::text, array['ACCIDENT_DRAFT_READ', 'ACCIDENT_DRAFT_SUBMIT', 'ACCIDENT_DRAFT_MODIFY']
-  union select '事故登记'::text,
-    array['ACCIDENT_REGISTER_READ', 'ACCIDENT_REGISTER_SUBMIT', 'ACCIDENT_REGISTER_MODIFY', 'ACCIDENT_REGISTER_CHECK']
-  union select '事故登记汇总统计'::text,
-    array['ACCIDENT_REGISTER_READ', 'ACCIDENT_REGISTER_SUBMIT', 'ACCIDENT_REGISTER_MODIFY', 'ACCIDENT_REGISTER_CHECK']
+  select '事故报案'::text, array_agg(code) from bc_identity_role where code like 'ACCIDENT_DRAFT_%'
+  union select '事故登记'::text, array_agg(code) from bc_identity_role where code like 'ACCIDENT_REGISTER_%'
+  union select '事故登记汇总统计'::text, array_agg(code) from bc_identity_role where code like 'ACCIDENT_REGISTER_%'
 )
 insert into bc_identity_role_resource (rid, sid)
   select r.id, s.id
@@ -105,36 +103,35 @@ insert into bc_identity_role_resource (rid, sid)
   and not exists (select 0 from bc_identity_role_resource rs where rs.rid = r.id and rs.sid = s.id);
 
 -- 用户与角色的关联
-with captain_motorcade(cap_array) as (
+with motorcade_captain(codes) as (
   -- 一分、二分两个分公司各个车队的车队长
-  select regexp_split_to_array(string_agg(a.code,','),',') from bc_identity_actor a
-  inner join bs_motorcade m on m.principal_id = a.id and (m.name like '一分%' or m.name like '二分%')
-    and m.status_ = 0
-),
-cfg(role_code, user_codes) as (
-  -- 查询报案信息角色
+  select array_agg(a.code::text)
+  from bc_identity_actor a
+  inner join bs_motorcade m on (
+    m.principal_id = a.id and (m.name like '一分%' or m.name like '二分%') and m.status_ = 0
+  )
+), cfg(role_code, user_codes) as (
+  -- 事故报案-查询
   select 'ACCIDENT_DRAFT_READ'::text, array['baochengzongbu']
-  -- 上报案件信息角色
+  -- 事故报案-上报
   union select 'ACCIDENT_DRAFT_SUBMIT'::text,
-    array_cat(array['fenGongSi1AQY', 'fenGongSi2AQY'], (select cap_array from captain_motorcade))
-  -- 修改报案信息角色
-  union select 'ACCIDENT_DRAFT_MODIFY'::text,
-    array_cat(array['fenGongSi1AQY', 'fenGongSi2AQY'], (select cap_array from captain_motorcade))
+    array_cat(array['fenGongSi1AQY', 'fenGongSi2AQY'], (select codes from motorcade_captain))
+  -- 事故报案-修改
+  --union select 'ACCIDENT_DRAFT_MODIFY'::text,
+  --  array_cat(array['fenGongSi1AQY', 'fenGongSi2AQY'], (select codes from motorcade_captain))
 
-  -- 交通事故登记信息查询角色
-  union select 'ACCIDENT_REGISTER_READ'::text,
-    array_cat((select cap_array from captain_motorcade), array['fenGongSi1AQY', 'fenGongSi2AQY', 'fenGongSi1Manager',
-      'fenGongSi2Manager', 'anquanguanlizu', 'yingyunzongjian', 'cjl'])
-  -- 交通事故报案信息登记角色
+  -- 事故登记-查询
+  union select 'ACCIDENT_REGISTER_READ'::text, array['baochengzongbu']
+  -- 事故登记-提交
   union select 'ACCIDENT_REGISTER_SUBMIT'::text,
     array_cat(array['fenGongSi1AQY', 'fenGongSi2AQY', 'fenGongSi1Manager', 'fenGongSi2Manager'],
-      (select cap_array from captain_motorcade))
-  -- 交通事故登记信息修改角色
+      (select codes from motorcade_captain))
+  -- 事故登记-审核
+  union select 'ACCIDENT_REGISTER_CHECK'::text, array['anquanguanlizu']
+  -- 事故登记-修改
   union select 'ACCIDENT_REGISTER_MODIFY'::text,
     array_cat(array['fenGongSi1AQY', 'fenGongSi2AQY', 'fenGongSi1Manager', 'fenGongSi2Manager'],
-      (select cap_array from captain_motorcade))
-  -- 交通事故登记信息审核角色
-  union select 'ACCIDENT_REGISTER_CHECK'::text, array['anquanguanlizu']
+      (select codes from motorcade_captain))
 )
 insert into bc_identity_role_actor (rid, aid)
   select r.id, a.id
@@ -144,14 +141,14 @@ insert into bc_identity_role_actor (rid, aid)
 
 -- 微服务配置: 选项配置
 -- select * from bc_option_item where pid in (select id from bc_option_group where key_ like 'micro-service%');
--- delete from bc_option_item where key_ in ('accident', 'file') and pid in (select id from bc_option_group where key_ like 'micro-service%');
+delete from bc_option_item where key_ in ('accident', 'file') and pid in (select id from bc_option_group where key_ like 'micro-service%');
 with cfg(sn, pkey, key, address, name) as (
   -- 交通事故
-  select '1101', 'micro-service', 'accident', 'http://127.0.0.1:9102/accident', '交通事故'
-  union select '1102', 'micro-service-internet', 'accident', 'http://gftaxi.cn:9102/accident', '交通事故'
+         select '1101', 'micro-service',          'accident', 'http://127.0.0.1:9102/accident',     '交通事故'
+  union select '1102', 'micro-service-internet', 'accident', 'http://www.gftaxi.cn:8081/accident', '交通事故'
   -- 文件服务器
-  union select '1103', 'micro-service', 'file', 'http://127.0.0.1:9013/file', '文件服务器'
-  union select '1104', 'micro-service-internet', 'file', 'http://gftaxi.cn:9013/file', '文件服务器'
+  union select '1103', 'micro-service',          'file',     'http://127.0.0.1:9013/file',         '文件服务器'
+  union select '1104', 'micro-service-internet', 'file',     'http://www.gftaxi.cn:8081/file',     '文件服务器'
 )
 insert into bc_option_item(id, status_, key_, value_, order_, pid)
   select nextval('core_sequence'), 0, c.key, c.address::text || ' | ' || c.name::text, c.sn
@@ -159,3 +156,12 @@ insert into bc_option_item(id, status_, key_, value_, order_, pid)
   from cfg c where not exists (
     select 0 from bc_option_item where key_ = c.key::text and pid = (select id from bc_option_group where key_ = c.pkey::text)
   );
+
+-- 测试环境的特殊权限配置：周文飞、开发组赋予全部事故权限
+insert into bc_identity_role_actor (rid, aid)
+  select r.id, a.id
+  from bc_identity_role r, bc_identity_actor a
+  where r.code like 'ACCIDENT_%' and r.code not like 'ACCIDENT_%_READ'
+  and a.code in ('fei', 'DevelopmentGroup')
+  and not exists (select 0 from bc_identity_role_actor ra where ra.aid = a.id and ra.rid = r.id)
+  order by a.code;
