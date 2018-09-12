77e3969f6e13d0caf3ae9d78efa0a7d52881bcad
RJ.HWang <rongjihuang@gmail.com>
2018-09-12 15:35:11 +0800

    事故报告视图 Dao 接口设计

---
+++ gftaxi-traffic-accident-data/src/main/kotlin/cn/gftaxi/traffic/accident/dao/AccidentReportDao.kt
@@ -0,0 +1,24 @@
+package cn.gftaxi.traffic.accident.dao
+
+import cn.gftaxi.traffic.accident.dto.AccidentReportDto4View
+import cn.gftaxi.traffic.accident.po.AccidentReport.Status
+import org.springframework.data.domain.Page
+import reactor.core.publisher.Mono
+
+/**
+ * 事故报告 Dao。
+ *
+ * @author RJ
+ */
+interface AccidentReportDao {
+  /**
+   * 获取指定状态的案件分页信息。
+   *
+   * 返回的信息按事发时间逆序排序，模糊搜索车队、车号、司机、编号。
+   *
+   * @param[statuses] 案件状态，为 null 则不限定
+   * @param[search] 模糊搜索的条件值，为空则忽略
+   */
+  fun find(pageNo: Int = 1, pageSize: Int = 25, statuses: List<Status>? = null, search: String? = null)
+    : Mono<Page<AccidentReportDto4View>>
+}
