6044e7dfe4a482ae664602f01c26e2076ac60529
RJ.HWang <rongjihuang@gmail.com>
2018-09-13 16:48:49 +0800

    Polishing

--- gftaxi-traffic-accident-rest-webflux/src/test/kotlin/cn/gftaxi/traffic/accident/rest/webflux/TestUtils.kt
+++ gftaxi-traffic-accident-rest-webflux/src/test/kotlin/cn/gftaxi/traffic/accident/rest/webflux/TestUtils.kt
@@ -7,22 +7,30 @@ import java.time.OffsetDateTime
  * @author RJ
  */
 object TestUtils {
+  private var strMap = hashMapOf("粤A." to 100000)
+  /** 生成带固定前缀的唯一字符串 */
+  fun random(prefix: String = "T"): String {
+    if (!strMap.containsKey(prefix)) strMap[prefix] = 1
+    else strMap[prefix] = strMap[prefix]!! + 1
+    return "$prefix${strMap[prefix]}"
+  }
+
   fun randomAccidentDraft(id: Int? = null, code: String): AccidentDraft {
     return AccidentDraft(
       id = id,
       code = code,
       status = AccidentDraft.Status.Todo,
-      carPlate = "car",
-      driverName = "driver",
+      carPlate = random("car"),
+      driverName = random("driver"),
       happenTime = OffsetDateTime.now(),
       createTime = OffsetDateTime.now(),
-      location = "location",
-      hitForm = "hitForm",
-      hitType = "hitType",
+      location = random("location"),
+      hitForm = random("hitForm"),
+      hitType = random("hitType"),
       overdueCreate = false,
-      source = "source",
-      authorName = "authorName",
-      authorId = "authorId"
+      source = random("source"),
+      authorName = random("authorName"),
+      authorId = random("authorId)")
     )
   }
 }
