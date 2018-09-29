52d15b10130ebcc7ef13a475fb42ab4005683195
RJ.Hwang <rongjihuang@gmail.com>
2018-09-30 00:00:26 +0800

    Try something

--- gftaxi-traffic-accident-data-jpa/src/main/kotlin/cn/gftaxi/traffic/accident/dao/jpa/repository/AccidentCaseJpaRepository.kt
+++ gftaxi-traffic-accident-data-jpa/src/main/kotlin/cn/gftaxi/traffic/accident/dao/jpa/repository/AccidentCaseJpaRepository.kt
@@ -6,6 +6,7 @@ import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Query
 import java.time.OffsetDateTime
+import java.util.*
 
 /**
  * 案件信息 Repository 接口。
@@ -17,4 +18,17 @@ interface AccidentCaseJpaRepository : JpaRepository<AccidentCase, Int> {
 
   @Query("select code from AccidentCase where code like ?1% order by code desc")
   fun getMaxCode(codePrefix: String, pageable: Pageable = PageRequest.of(0, 1)): List<String>
+
+  @Query("select code from AccidentCase where id = ?1")
+  fun findCodeById(id: Int): String
+
+  @Query("select code from AccidentCase where id = ?1")
+  fun findCodeById2(id: Int): Optional<String>
+
+  @Query("select code from AccidentCase where code like ?1% order by code desc")
+  fun findTopCode(codePrefix: String, pageable: Pageable = PageRequest.of(0, 1)): List<CodeOnly>
+}
+
+interface CodeOnly {
+  val code: String?
 }
--- gftaxi-traffic-accident-data-jpa/src/test/kotlin/cn/gftaxi/traffic/accident/dao/jpa/draft/GetDraftMethodImplTest.kt
+++ gftaxi-traffic-accident-data-jpa/src/test/kotlin/cn/gftaxi/traffic/accident/dao/jpa/draft/GetDraftMethodImplTest.kt
@@ -12,10 +12,13 @@ import cn.gftaxi.traffic.accident.dto.AccidentDraftDto4Form
 import cn.gftaxi.traffic.accident.test.TestUtils.randomCase
 import cn.gftaxi.traffic.accident.test.TestUtils.randomInt
 import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
+import org.springframework.dao.EmptyResultDataAccessException
 import org.springframework.test.context.junit.jupiter.SpringJUnitConfig
 import reactor.test.StepVerifier
 import java.time.OffsetDateTime
@@ -33,6 +36,44 @@ class GetDraftMethodImplTest @Autowired constructor(
   private val situationRepository: AccidentSituationJpaRepository,
   private val dao: AccidentDao
 ) {
+  @Test
+  fun `Try Single Column`() {
+    // empty
+    assertThrows<EmptyResultDataAccessException> { caseRepository.findCodeById(randomInt()) }
+    assertFalse(caseRepository.findCodeById2(randomInt()).isPresent)
+    assertTrue(caseRepository.findTopCode("code").isEmpty())
+
+    // init data
+    val (case, _) = randomCase(
+      id = null,
+      code = "code1",
+      happenTime = OffsetDateTime.now(),
+      stage = CaseStage.Drafting,
+      overdueDraft = false,
+      draftStatus = DraftStatus.Drafting,
+      registerStatus = AuditStatus.ToSubmit
+    )
+    caseRepository.save(case)
+    assertEquals(case.code, caseRepository.findCodeById(case.id!!))
+    assertEquals(case.code, caseRepository.findCodeById2(case.id!!).get())
+
+    val (case2, _) = randomCase(
+      id = null,
+      code = "code2",
+      happenTime = OffsetDateTime.now(),
+      stage = CaseStage.Drafting,
+      overdueDraft = false,
+      draftStatus = DraftStatus.Drafting,
+      registerStatus = AuditStatus.ToSubmit
+    )
+    caseRepository.save(case2)
+    val c = caseRepository.findTopCode("code")
+    val f = c.first()
+    println(f)
+    assertEquals(1, c.size)
+    assertEquals(case2.code, caseRepository.findTopCode("code").first().code)
+  }
+
   @Test
   fun `Get empty`() {
     StepVerifier.create(dao.getDraft(randomInt())).verifyComplete()
